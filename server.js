74f242d69e8d7d4e041503395907deebcd029eb0
umairzidi2575-lgtm <umairzidi2575@gmail.com>
2026-01-13 19:24:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get("/digilocker/auth", (req, res) => {
-  res.send("Backend working ✅");
+app.get("/digilocker/callback", (req, res) => {
+  // OTP ke baad DigiLocker yahin aata hai
+  res.redirect("https://umairzidi2575-lgtm.github.io/loan-page/");
 });
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
+
